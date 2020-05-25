b2b05d2680b8c2b7ce40a7665d716a44201fdc30
Dalin  Oluoch <dc_dalin@Dalins-MacBook-Pro.local>
2020-05-25 19:20:57 +0300

    Change backend setup

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,29 +1,57 @@
+import { createServer } from "http";
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
 
 import nextApp from "@fiklin/client";
-import apolloServer from "@fiklin/graphql";
+import typeDefs from "./typeDefs";
+import resolvers from "./resolvers";
 
-const { PORT } = process.env;
+const { PORT, NODE_ENV } = process.env;
 
-async function main() {
-  const app = express();
+(async () => {
+  try {
+    const app = express();
 
-  await bootstrapApolloServer(app);
-  await bootstrapClientApp(app);
+    app.disable("x-powered-by");
 
-  app.listen(PORT, (err) => {
-    if (err) throw err;
-    console.log(`[ server ] ready on port ${PORT}`);
-  });
-}
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+      context: async ({ req, res, connection }) => {
+        if (connection) {
+          // check connection for metadata
+          return connection.context;
+        } else {
+          return { req, res };
+        }
+      },
+      introspection: true,
+      playground: NODE_ENV !== "production",
+    });
+
+    server.applyMiddleware({ app, cors: false });
+
+    const httpServer = createServer(app);
+
+    server.installSubscriptionHandlers(httpServer);
+
+    await bootstrapClientApp(app);
+
+    httpServer.listen({ port: PORT }, () =>
+      console.log(
+        `
+      ################################################
+      🚀 Server ready at http://localhost:${PORT}${server.graphqlPath} 🚀
+      ################################################
+      `
+      )
+    );
+  } catch (e) {
+    console.error(e);
+  }
+})();
 
 async function bootstrapClientApp(expressApp) {
   await nextApp.prepare();
   expressApp.get("*", nextApp.getRequestHandler());
 }
-
-async function bootstrapApolloServer(expressApp) {
-  apolloServer.applyMiddleware({ app: expressApp });
-}
-
-main();
