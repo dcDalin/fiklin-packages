1a0c40683ddef8e7277b86f536010156d0aa0774
Dalin  Oluoch <dc_dalin@Dalins-MacBook-Pro.local>
2020-06-11 22:30:31 +0300

    Changes

--- packages/client/components/Layout.tsx
+++ packages/client/components/Layout.tsx
@@ -1,21 +1,52 @@
-import Link from 'next/link';
+import Link from "next/link";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
-const Layout = (props) => (
-  <div>
-    <ul>
-      <li>
-        <Link href="/">
-          <a>No SSR</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/ssr">
-          <a>SSR</a>
-        </Link>
-      </li>
-    </ul>
-    {props.children}
-  </div>
-);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const Layout = (props) => {
+  const classes = useStyles();
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            <Link href="/ssr">
+              <a>SSR</a>
+            </Link>
+          </Typography>
+          <Link href="/">
+            <a>No SSR</a>
+          </Link>
+        </Toolbar>
+      </AppBar>
+
+      {props.children}
+    </>
+  );
+};
 
 export default Layout;
--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -1,31 +1,11 @@
 import React from "react";
-import { useQuery } from "@apollo/react-hooks";
 import Layout from "../components/Layout";
-import gql from "graphql-tag";
 import { withApollo } from "../apollo/apollo";
-import { Button } from "@material-ui/core";
-
-const QUERY = gql`
-  query getUser {
-    user {
-      name
-    }
-  }
-`;
 
 const NOSSR = () => {
-  const { data, loading, error, refetch } = useQuery(QUERY);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
-
   return (
     <Layout>
       <h1>This should be rendered on client side</h1>
-      <pre>Data: {data.user.name}</pre>
-      <Button color="primary" onClick={() => refetch()}>
-        Hello World
-      </Button>
     </Layout>
   );
 };
